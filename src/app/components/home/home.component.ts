1817d7ceff5d1e9c74ff30e0c20010684f200d08
Mmy26 <yu.b1994masamura@gmail.com>
2025-10-01 19:34:50 +0900

    フォームの色を動的に変更

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,11 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  public status: string = 'NG';
+  public statuses: string[] = [];
+  public initialStatuses: string[] = [];
+  public items: any[] = Array(5).fill(0); // 5つのフォームを作成するためのダミー配列
 
   constructor() { }
 
   ngOnInit(): void {
+    // 初期値を設定
+    this.items.forEach((_, index) => {
+      const initialValue = index % 2 === 0 ? 'OK' : 'NG'; // 例として初期値を交互に設定
+      this.statuses.push(initialValue);
+      this.initialStatuses.push(initialValue);
+    });
   }
 
+  isChanged(index: number): boolean {
+    return this.statuses[index] !== this.initialStatuses[index];
+  }
 }
