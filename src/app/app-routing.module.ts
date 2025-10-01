122da920040988e8c3775e7a246eec208ec6d7c9
Mmy26 <yu.b1994masamura@gmail.com>
2025-10-01 19:23:11 +0900

    selectフォームの追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+  {redirectTo: 'home', pathMatch: 'full', path: ''},
+  {
+    path: 'home',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
